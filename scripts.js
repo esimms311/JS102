d6ca5f19ce08d86e0a775c9baa0466e59480df28
esimms311 <eric.simmons26@gmail.com>
2017-03-09 12:43:51 -0700

    JS practice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
 //Use this file to implement Part One of your project
 
+var animals = {
+  dog: 'woof',
+  cat: 'meow',
+  llama: "ruff",
+  dragon: 'fire',
+  baby: 'cry'
+}
 
+var names = {}
+
+names.boys = 'tucker';
+names.girls = 'sofia';
+
+animals['bird'] = 'penguin';
+console.log(names.boys);
+console.log(animals.llama);
+
+
+var count = 0;
+for (var key in names) {
+  count++;
+  if(key === 'boys') {
+    console.log("hi my name is " + names[key]);
+  } else if ( key === 'girls') {
+    console.log('hi my name is ' + names[key]);
+  }
+}
+
+console.log(count);
